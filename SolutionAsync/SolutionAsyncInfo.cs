2c79906468cfc63c1be00da85d14522677509aa9
秋水 <1123993881@qq.com>
2022-02-01 15:12:25 +0800

    0.9.1 release.

--- SolutionAsync/SolutionAsyncInfo.cs
+++ SolutionAsync/SolutionAsyncInfo.cs
@@ -23,6 +23,6 @@ namespace SolutionAsync
         //Return a string representing your preferred contact details.
         public override string AuthorContact => "201704110219@stu.zafu.edu.cn";
 
-        public override string Version => "0.9.0";
+        public override string Version => "0.9.1";
     }
 }
